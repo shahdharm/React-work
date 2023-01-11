487013f0c8108140e0bb659ebbe54b05d8dabe29
ASUS <shahdharmendrakumar209@gmail.com>
2023-01-11 09:54:41 +0545

    fourth

--- src/App.js
+++ src/App.js
@@ -33,26 +33,12 @@ function App(props) {
     setNewNote('')
   }
   const handleDelete = (id) =>{
-    window.confirm(`Do you really want to delete note with id ${id}`)
+    if(window.confirm(`Do you really want to delete note with id ${id}`)){
+      setNotes(notes.filter(n => n.id !== id))
+    }
   }
- 
-  // const [counter, setCounter] = useState(0)
-  // // setTimeout(()=> setCounter(counter +1),100)
- 
-  // console.log('Rendering ${counter}...')
- 
-  // const handlePress = ()=>{
-  //   setCounter(counter+1)
-  // }
   return (
   <>
-     {/* <h2>Hello reactjs</h2>
-     <h2>{counter}</h2>
-     <button
-     onClick={()=> setCounter(counter+100000)}
-     >Press</button>
-     <Feedback/> */}
- 
     <h2>Notes</h2>
     <button onClick={() => setShowAll(!ShowAll)}>
       {ShowAll ? 'show important': 'show all'}
@@ -70,30 +56,8 @@ function App(props) {
       <input value = {newNote} onChange ={handleInputChange}/>
       <button onClick={handleAdd}>add</button>
     </form>
-
- 
- 
   </>  
   )
- 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
- 
- 
 }
 export default App;
  
