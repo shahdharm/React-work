91ee892194bb9a0e55c92df377672bd3e86abb33
ASUS <shahdharmendrakumar209@gmail.com>
2023-01-13 12:53:50 +0545

    fifth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
- 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // Firts we need to import Feedback
 // import Feedback from './components/Feedback'
  
 function App(props) {
   // const {notes} = props.notes
   const [newNote, setNewNote] = useState('Enter your name')
-  const [notes, setNotes ] = useState(props.notes)
+  const [notes, setNotes ] = useState([])
   const [ShowAll, setShowAll] = useState(true)
+
+  useEffect(() =>{
+    axios.get('http://localhost:3001/notes')
+    .then(response =>{
+      console.log(response)
+      setNotes(response.data)
+    })
+    .catch(err => console.log(err))
+  },[])
 
   const notesToShow = ShowAll
   ? notes
@@ -24,17 +33,35 @@ function App(props) {
     event.preventDefault()
     // alert('Testing')
     const note ={
-      id: notes.length + 1,
+      // id: notes.length + 1,
       content: newNote,
       date: new Date().toString(),
       impotant: Math.random() <0.5
     }
+
+    axios.post('http://localhost:3001/notes', note)
+    .then(response =>{
+      console.log(response)
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+
+    })
+    .catch(err => console.log(err))
+
     if(newNote !== '')setNotes(notes.concat(note))
     setNewNote('')
   }
   const handleDelete = (id) =>{
     if(window.confirm(`Do you really want to delete note with id ${id}`)){
-      setNotes(notes.filter(n => n.id !== id))
+      axios.delete(`http://localhost:3001/notes/ ${id}`)
+      .then(response =>{
+        console.log(response)
+      
+        setNotes(notes.filter(n => n.id !==id))
+      })
+      .catch(err => console.log(err))
+     
+       
     }
   }
   return (
--- src/index.js
+++ src/index.js
@@ -6,33 +6,34 @@ import reportWebVitals from './reportWebVitals';
 
  
 
-    const notes= [
-      {
-        "id": 1,
-        "content": "HTML is easy",
-        "date": "2022-1-17T17:30:31.098Z",
-        "important": true
-      },
-      {
-        "id": 2,
-        "content": "Browser can execute only JavaScript",
-        "date": "2022-1-17T18:39:34.091Z",
-        "important": false
-      },
-      {
-        "id": 3,
-        "content": "GET and POST are the most important methods of HTTP protocol",
-        "date": "2022-1-17T19:20:14.298Z",
-        "important": false
-      }
-    ]
+    // const notes= [
+      // {
+      //   "id": 1,
+      //   "content": "HTML is easy",
+      //   "date": "2022-1-17T17:30:31.098Z",
+      //   "important": true
+      // },
+      // {
+      //   "id": 2,
+      //   "content": "Browser can execute only JavaScript",
+      //   "date": "2022-1-17T18:39:34.091Z",
+      //   "important": false
+      // },
+      // {
+      //   "id": 3,
+      //   "content": "GET and POST are the most important methods of HTTP protocol",
+      //   "date": "2022-1-17T19:20:14.298Z",
+      //   "important": false
+      // }
+    // ]
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
  
 root.render(
   // <React.StrictMode>
-    <App notes = {notes}/>
+    <App/>
+    // notes = {notes}/
   /* </React.StrictMode> */
 );
 
